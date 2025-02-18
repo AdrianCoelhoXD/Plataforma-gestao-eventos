897e871d781ab1116de9a7b57ccaa7b9f9145948
Adrian Coelho <adriantwocoelho2010@outlook.com>
2025-02-18 03:17:42 -0300

    Corrigido esquema GraphQL para eventos: alterado "event" para "Event" e corrigido erro de digitação em "title"

--- schemas/eventSchema.js
+++ schemas/eventSchema.js
@@ -1,39 +1,40 @@
-const { buildSchema } = required ('graphql');
+const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-    type User{
-    id: ID!
-    name: String!
-    email: String!
-    role: String!
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        role: String!
     }
 
-    type event{
-    id: ID!
-    title: String!
-    description: String!
-    date: String!
-    location: String!
-    online: Boolean!
-    maxParticipants: Int!
-    participants: [User!]!
-    organizer: User!
-    
-    }
-    type Query{
-    events: [Event!]!   
-    event(id: ID!): Event
+    type Event {  # Aqui estava "event", agora está com "E" maiúsculo
+        id: ID!
+        title: String!  # Corrigido "tittle" para "title"
+        description: String!
+        date: String!
+        location: String!
+        online: Boolean!
+        maxParticipants: Int!
+        participants: [User!]!
+        organizer: User!
     }
 
-    type Mutation{
-    createEvent(
-    tittle: String!
-    description: String!
-    date: String!
-    location: String!
-    online: Boolean!
-    maxParticipants: Int!
-    ): Event!
-}
-    `);
+    type Query {
+        events: [Event!]!   
+        event(id: ID!): Event
+    }
+
+    type Mutation {
+        createEvent(
+            title: String!  # Corrigido "tittle" para "title"
+            description: String!
+            date: String!
+            location: String!
+            online: Boolean!
+            maxParticipants: Int!
+        ): Event!
+    }
+`);
+
 module.exports = schema;
