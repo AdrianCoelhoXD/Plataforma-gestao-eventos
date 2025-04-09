2f0802491b1e72549e3fbbca2898c67138718c06
Adrian Coelho <adriantwocoelho2010@outlook.com>
2025-04-09 04:13:02 -0300

    fix

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -11,16 +11,3 @@ router.delete('/:id', auth, eventController.deleteEvent);
 router.patch('/:id', auth, eventController.updateEvent);
 
 module.exports = router;
-// const eventValidator = require('../utils/validators');
-
-// // Criar um novo evento
-// router.post('/', auth, eventController.createEvent);
-
-// // Listar todos os eventos (para a página Home)
-// router.get('/', eventController.getAllEvents);
-
-// // Listar eventos de um organizador específico
-// router.get('/organizer/:id',  eventController.getOrganizerEvents);
-
-// // Listar eventos do organizador autenticado
-// router.get('/organizer/me', eventController.getMyEvents);
